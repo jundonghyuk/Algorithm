64be597499cb774f60e7eec85fed3c5202a1795d
JunDonghyuk <ehdgur4814@naver.com>
2023-04-18 13:51:58 +0900

    BOJ_11399

---
+++ Do_It_Algorithm_CodingTest/src/Sorting/BOJ_11399/Main.java
@@ -0,0 +1,26 @@
+package Sorting.BOJ_11399;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = 0; i < N - 1; i++) {
+            arr[i + 1] += arr[i];
+        }
+        for (int i = 0; i < N; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
